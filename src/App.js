be8d4fce52f2fa91fae3f23b025af4c93cc2ff17
Antip-An <anechka-antipina@mail.ru>
2021-11-11 16:00:58 +0300

    https://github.com/Antip-An/frontend.git

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header/Header";
 import Main from './pages/Main'
 import Portfolio from './pages/Portfolio'
+import Service from './pages/Service'
+import Profile from './pages/Profile'
 import Content from './Content'
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Main/>}/>
           <Route path='/portfolio' element={<Portfolio/>}/>
+          <Route path='/service' element={<Service/>}/>
+          <Route path='/profile' element={<Profile/>}/>
         </Routes>
       </Content>
     </Router>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,8 +6,10 @@ function Header() {
   return (
     <>
       <div className="header">
+        <img src="./images/0.PNG"></img>
         <Link to="/">Главная</Link>
         <Link to="/portfolio">Портфолио</Link>
+        <Link to="/service">Услуги</Link>
       </div>
     </>
   );
